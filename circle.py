688f117acd5ece4763026941919edede9d1aee38
andrel1912 <andreas.holmberg@stud.sti.se>
2025-11-07 01:30:01 +0100

    Shape/Circle

--- circle.py
+++ circle.py
@@ -1,1 +1,28 @@
-#Aasdawd
+from shape import Shape
+import math
+
+class Circle(Shape):
+    def __init__(self, x=0, y=0, radius=1):
+# Använder Shape för position
+        super().__init__(x, y)
+
+# Kollar så att radien är positiv
+        if radius <= 0:
+            raise ValueError("Radius must be positive")
+        self.radius = radius
+
+    @property
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+# Omkrets
+    @property
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+    def is_unit_circle(self):
+        return self.radius == 1 and self .x == 0 and self.y == 0
+    
+# utskrift/felsökning i terminalen
+    def __repr__(self):
+        return f"Circle(x={self.x}, y={self.y}, radius={self.radius})"
--- shape.py
+++ shape.py
@@ -1,2 +1,32 @@
-class = Shape:
+class Shape:
+# class för formerna
     def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+    
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+    
+# Flyttar formen
+    def move(self , dx, dy):
+        self.x += dx
+        self.y += dy
+
+# Jämförelser baserat på arean 
+    def __eq__(self, other):
+        return self.area() == other.area()
+    
+    def __lt__(self, other):
+        return self.area() < other.area()
+    
+    def __le__(self, other):
+        return self.area() <= other.area()
+    
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
